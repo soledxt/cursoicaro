851d3c7f12fd4a7eceddf60ef2e3e0bde8594d69
soledad <soledixit@gmail.com>
2021-02-21 21:45:45 -0300

    Subida de los ejercicios 1

---
+++ Shell/soledad1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+string_a="Linux"
+string_b="GENIAL"
+
+echo "Son $string_a y $string_b el mismo string?"
+[ $string_a = $string_b ]
+echo $?
+
+
+num_a=100
+num_b=100
+
+echo "Es $num_a igual a $num_b ?"
+[ $num_a -eq $num_b ]
+echo $?
